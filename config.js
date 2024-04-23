2889d52214e0cc975edc122a933f0f4afa6765dc
ManzKeDua <141548199+ManzKeDua@users.noreply.github.com>
2024-04-23 21:51:04 +0700

    Update and rename config..js to config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Setting Bot
-global.botToken = 'Your Bot Token';
+global.botToken = '6442156581:AAE1r5mek2j9UkJwXfU_lQmdEO6gWn1sTrA';
 global.api = 'https://itzpire.site';
 global.qrisImage = './qris.jpg'
 global.toxic = "true"; // true or false for simi
